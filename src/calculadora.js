a27808a474f8cf1f9c8f58c4836ceeb5406c138b
Heitor Silva <heitor.silva@corp.globo.com>
2018-04-26 12:10:30 -0300

    Adiciona src/classes.js

---
+++ src/calculadora.js
@@ -0,0 +1,7 @@
+class Calculadora {
+  static somar(a, b) {
+    return a + b;
+  }
+}
+
+export default Calculadora;
---
+++ src/classes.js
@@ -0,0 +1,38 @@
+console.info('CLASSES');
+
+class Veiculo {
+  constructor(marca, modelo) {
+    this.marca = marca;
+    this.modelo = modelo;
+  }
+
+  buzinar() {
+    return 'FOOOOOOOOMMMMM';
+  }
+}
+
+class Carro extends Veiculo {
+  constructor(marca, modelo) {
+    super(marca, modelo);
+    this.rodas = 4;
+  }
+}
+
+class Moto extends Veiculo {
+  constructor(marca, modelo) {
+    super(marca, modelo);
+    this.rodas = 2;
+  }
+}
+
+const carro = new Carro('Volkswagen', 'Gol');
+
+console.log(carro.marca, carro.modelo, carro.rodas, carro.buzinar());
+
+const moto = new Moto('Harley Davidson', 'Street Bob');
+
+console.log(moto.marca, moto.modelo, moto.rodas, moto.buzinar());
+
+import Calculadora from './calculadora';
+
+console.log(`Resultado da soma: ${Calculadora.somar(2, 2)}`); // 4
