6eb643657a9549138758c73e59a656b20ee2a6cf
Heitor Silva <heitor.silva@corp.globo.com>
2018-04-26 10:56:16 -0300

    Adiciona src/restParams.js

---
+++ src/restParams.js
@@ -0,0 +1,10 @@
+console.info('REST PARAMS');
+
+function minhaFuncao(primeiro, ...lista) {
+  console.log('Primeiro parâmetro:', primeiro);
+  console.log('Lista de parâmetros:', lista);
+}
+
+minhaFuncao('Olá', 'Mundo');
+
+minhaFuncao('Olá', 'Mundo', 'Incrível');
